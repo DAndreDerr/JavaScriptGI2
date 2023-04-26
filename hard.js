0bbb7faff1151c133fd5f6fbf5bbd3eb56fa2d3c
DAndreDerr <dandre020727@gmail.com>
2023-04-26 13:14:11 -0400

    updated hard challenge

--- hard.js
+++ hard.js
@@ -1,11 +1,27 @@
-let tomMass = 8;
-let tomHeight = 9;
-let jerryMass = 45;
-let jerryHeight = 10;
+let tomMass = 8; //8 grams
+let tomHeight = 9; //9 inches
+let jerryMass = 45; //45 grams
+let jerryHeight = 10; //10 inches
 
+// this function converts inches to meters
+function inchToMeters(x) {
+    return x / 39.37;
+}
+
+// this function converts grams to kilograms
+function gramsToKilograms(x) {
+    return x / 1000;
+}
+
+// this function calculates BMI using the formula BMI = mass / (height * height) (mass in kg and height in meter).
 function bmi(m, h) {
     return m / (h * h);
 }
 
-let tomHigherBMI = bmi(tomMass, tomHeight) > bmi(jerryMass, jerryHeight);
+let tomMassConverted = gramsToKilograms(tomMass);
+let tomHeightConverted = inchToMeters(tomHeight)
+let jerryMassConverted = gramsToKilograms(jerryMass);
+let jerryHeightConverted =inchToMeters(jerryHeight)
+
+let tomHigherBMI = bmi(tomMassConverted, tomHeightConverted) > bmi(jerryMassConverted, jerryHeightConverted);
 console.log(`Is Tom's BMI higher than Jerry's? ${tomHigherBMI}`);
